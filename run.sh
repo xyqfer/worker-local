20f8418c59551a6c5fe705b747574cb8c9e8e543
凉凉 <xyqfer@gmail.com>
2022-03-04 15:57:23 +0800

    test

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-pm2 start "miniflare index.js" --no-daemon && pm2 start "miniflare index2.js -p 8989" --no-daemon
+pm2 start "miniflare index.js" --no-daemon
+
+pm2 start "miniflare index2.js -p 8989" --no-daemon
